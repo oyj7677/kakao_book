371abdfb67dfc53a7af0e0615dfa22a0a33dceb9
OYJ <oyj7677@gmail.com>
2025-09-09 21:33:04 +0900

    [Fix] 직렬화 사양 누락 수정

--- di/src/main/kotlin/com/oyj/di/database/DbConverter.kt
+++ di/src/main/kotlin/com/oyj/di/database/DbConverter.kt
@@ -1,16 +1,18 @@
 package com.oyj.di.database
 
 import androidx.room.TypeConverter
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
 
 class DbConverter {
     @TypeConverter
     fun fromList(list: List<String>?): String {
-        return if (list == null) "" else Json.encodeToString(list)
+        return if (list == null) "" else Json.encodeToString(ListSerializer(String.serializer()), list)
     }
 
     @TypeConverter
     fun toList(value: String): List<String> {
-        return if (value.isBlank()) emptyList() else Json.decodeFromString(value)
+        return if (value.isBlank()) emptyList() else Json.decodeFromString(ListSerializer(String.serializer()), value)
     }
 }
